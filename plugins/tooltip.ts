d549595ead29d28f7514c94aa8261077b290de5e
Vincent GUILLOIS <vincent@helloasso.org>
2025-01-13 09:32:05 +0100

    fix

--- plugins/tooltip.ts
+++ plugins/tooltip.ts
@@ -8,10 +8,19 @@ export default defineNuxtPlugin((nuxtApp) => {
       const parent = el.cloneNode(false)
 
       el.replaceWith(parent)
+
+        // Traiter correctement vnode.children
+        const children =
+        Array.isArray(vnode.children)
+          ? vnode.children
+          : vnode.children
+          ? [vnode.children] // Mettre dans un tableau s'il s'agit d'un objet ou d'une chaîne
+          : [];
+
       //parent.appendChild(el)
       const tooltipElement = h(Tooltip, {
       }, {
-        default: () => vnode.children || [], // array with children else it break the devtools
+        default: () => children, // array with children else it break the devtools
         tooltip: () => binding.value
       })
       //el.remove()
