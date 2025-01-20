c446a4d75a2dff58b5cd5fdd791e44e027093f6a
Vincent GUILLOIS <vincent@helloasso.org>
2025-01-20 22:51:24 +0100

    wip

--- components/Button.vue
+++ components/Button.vue
@@ -358,6 +358,12 @@ const handleClick = (evt: Event) => {
 .button-group .button {
   @apply m-0 last-of-type:rounded-[0_12px_12px_0] first-of-type:rounded-[12px_0_0_12px];
 }
+.button-group .button.button--transparent {
+  @apply border-x-1;
+}
+.button-group .button.button--transparent:nth-last-child() {
+  @apply border-r-1;
+}
 .button-group .button:not(:last-of-type):not(:first-of-type) {
   @apply rounded-none border-0;
 }
