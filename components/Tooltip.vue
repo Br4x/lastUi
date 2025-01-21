c2d5811d31ca761b251f04eb08582d3257d18a7c
Vincent GUILLOIS <vincent@helloasso.org>
2025-01-21 13:32:37 +0100

    fix tooltip with directive

--- components/Tooltip.vue
+++ components/Tooltip.vue
@@ -1,64 +1,75 @@
 <template>
-  <div
-    class="tooltip-content"
-    ref="contentRef"
-    @mouseenter="!notHover && handlerMouseEnter()"
-    @mouseleave="!notHover && handleMouseLeave()"
-  >
+  <div v-if="!viaDirective" class="tooltip-content" ref="contentRef" @mouseenter="!notHover && handlerMouseEnter()"
+    @mouseleave="!notHover && handleMouseLeave()">
     <Transition name="tooltip">
-      <div
-        v-if="activeTooltip"
-        class="tooltip component--primary"
-        ref="tooltipRef"
-        :class="[
-          { top: !bottom && !left && !right },
-          { bottom: bottom },
-          { left: left },
-          { right: right },
-          { shadow: shadow },
-          { notArrow: notArrow },
-          { square: square },
-          { circle: circle },
-          { border: border },
-          { borderThick: borderThick },
-          { loading: loading }, 
-          position
-        ]"
-        @mouseenter="interactivity && handleMouseEnterTooltip()"
-        @mouseleave="interactivity && handleMouseLeaveTooltip()"
-      >
+      <div v-if="activeTooltip" class="tooltip component--primary" ref="tooltipRef" :class="[
+        { top: !bottom && !left && !right },
+        { bottom: bottom },
+        { left: left },
+        { right: right },
+        { shadow: shadow },
+        { notArrow: notArrow },
+        { square: square },
+        { circle: circle },
+        { border: border },
+        { borderThick: borderThick },
+        { loading: loading },
+        position
+      ]" @mouseenter="interactivity && handleMouseEnterTooltip()"
+        @mouseleave="interactivity && handleMouseLeaveTooltip()">
         <slot name="tooltip" />
         <div v-if="loading" class="tooltip__loading" />
       </div>
     </Transition>
     <slot />
   </div>
+  <Transition v-if="viaDirective" name="tooltip">
+    <div v-if="activeTooltip" class="tooltip component--primary" ref="tooltipRef" :class="[
+      { top: !bottom && !left && !right },
+      { bottom: bottom },
+      { left: left },
+      { right: right },
+      { shadow: shadow },
+      { notArrow: notArrow },
+      { square: square },
+      { circle: circle },
+      { border: border },
+      { borderThick: borderThick },
+      { loading: loading },
+      position
+    ]" @mouseenter="interactivity && handleMouseEnterTooltip()" @mouseleave="interactivity && handleMouseLeaveTooltip()">
+      <slot name="tooltip" />
+      <div v-if="loading" class="tooltip__loading" />
+    </div>
+  </Transition>
 </template>
 
 <script setup>
 
 const props = defineProps({
   modelValue: null,
-  loading: { type: Boolean, default: false },
-  bottom: { type: Boolean, default: false },
-  left: { type: Boolean, default: false },
-  right: { type: Boolean, default: false },
-  notHover: { type: Boolean, default: false },
-  shadow: { type: Boolean, default: false },
-  interactivity: { type: Boolean, default: false },
-  notArrow: { type: Boolean, default: false },
-  square: { type: Boolean, default: false },
-  circle: { type: Boolean, default: false },
-  border: { type: Boolean, default: false },
-  borderThick: { type: Boolean, default: false },
+  loading: Boolean,
+  bottom: Boolean,
+  left: Boolean,
+  right: Boolean,
+  notHover: Boolean,
+  shadow: Boolean,
+  interactivity: Boolean,
+  notArrow: Boolean,
+  square: Boolean,
+  circle: Boolean,
+  border: Boolean,
+  borderThick: Boolean,
   delay: { type: Number, default: 0 },
   color: { type: String, default: null },
-  primary: { type: Boolean, default: false },
-  danger: { type: Boolean, default: false },
-  warn: { type: Boolean, default: false },
-  success: { type: Boolean, default: false },
-  dark: { type: Boolean, default: false },
-  position:{ type: String, default: 'top' } 
+  primary: Boolean,
+  danger: Boolean,
+  warn: Boolean,
+  success: Boolean,
+  dark: Boolean,
+  viaDirective: Boolean,
+  focusedElement: Object,
+  position: { type: String, default: 'top' }
 })
 
 const activeTooltip = ref(props.modelValue)
@@ -135,6 +146,12 @@ watch(() => props.modelValue, (val) => {
 })
 
 onMounted(() => {
+  if (props.viaDirective) {
+    contentRef.value = props.focusedElement
+    props.focusedElement.addEventListener('mouseenter', handleMouseEnterTooltip)
+    props.focusedElement.addEventListener('mouseleave', handleMouseLeaveTooltip)
+  }
+
   window.addEventListener('popstate', () => {
     document.querySelectorAll('.tooltip').forEach((tooltip) => {
       tooltip.remove()
@@ -146,60 +163,76 @@ onMounted(() => {
     window.addEventListener('mousedown', handleMouseDownNotHover)
     window.addEventListener('touchstart', handleMouseDownNotHover)
   }
- 
+
 })
 
 onBeforeUnmount(() => {
   activeTooltip.value = false
   window.removeEventListener('resize', handleResize)
   window.removeEventListener('mousedown', handleMouseDownNotHover)
+
+  if (props.viaDirective) {
+    props.focusedElement.removeEventListener('mouseenter', handleMouseEnterTooltip)
+    props.focusedElement.removeEventListener('mouseleave', handleMouseLeaveTooltip)
+  }
 })
 </script>
 
 <style scoped>
-
 .tooltip-enter-active,
 .tooltip-leave-active {
   @apply transition-all duration-[0.25s] ease-[ease];
 }
+
 .tooltip-enter,
 .tooltip-leave-to {
   @apply opacity-0;
 }
+
 .tooltip-enter.top,
 .tooltip-leave-to.top {
   @apply translate-y-[5px];
 }
+
 .tooltip-enter.bottom,
 .tooltip-leave-to.bottom {
   @apply translate-y-[-5px];
 }
+
 .tooltip-enter.left,
 .tooltip-leave-to.left {
   @apply translate-x-[5px];
 }
+
 .tooltip-enter.right,
 .tooltip-leave-to.right {
   @apply translate-x-[-5px];
 }
+
 .dark .tooltip {
   @apply bg-gray-400 text-white;
 }
+
 .dark .tooltip:not(.change-color) {
   @apply text-white;
 }
+
 .tooltip {
   @apply bg-gray-800 text-white absolute z-[1000000] text-center text-[0.85rem] max-w-[350px] min-w-[30px] min-h-[30px] px-2.5 py-[5px] rounded-xl;
 }
+
 .tooltip.loading {
   @apply text-white/20;
 }
+
 .tooltip.loading * {
   @apply opacity-50 pointer-events-none;
 }
+
 .tooltip__loading {
-  @apply w-[22px] h-[22px] absolute block -translate-x-2/4 -translate-y-2/4 opacity-100 rounded-[50%] left-2/4 top-2/4 after:( border-transparent border-r-white animate-[loadingTooltip_0.6s_ease_infinite] rounded-[inherit] border-2 border-solid) before:(border-transparent border-r-white animate-[loadingTooltip_0.6s_linear_infinite] rounded-[inherit] border-2 border-solid border-r-dashed );
+  @apply w-[22px] h-[22px] absolute block -translate-x-2/4 -translate-y-2/4 opacity-100 rounded-[50%] left-2/4 top-2/4 after:(border-transparent border-r-white animate-[loadingTooltip_0.6s_ease_infinite] rounded-[inherit] border-2 border-solid) before:(border-transparent border-r-white animate-[loadingTooltip_0.6s_linear_infinite] rounded-[inherit] border-2 border-solid border-r-dashed);
 }
+
 .tooltip__loading:after {
   animation: loadingTooltip 0.6s ease infinite;
 }
@@ -208,90 +241,112 @@ onBeforeUnmount(() => {
 .tooltip__loading:before {
   @apply content-[""] absolute w-full h-full box-border transition-all duration-[0.25s] ease-[ease] block shadow-[0_0_0_0] shadow-black;
 }
+
 .tooltip__loading:before {
   animation: loadingTooltip 0.6s linear infinite;
 }
+
 .tooltip.borderThick {
   @apply bg-white text-gray-800 shadow-[0_2px_10px_0] shadow-black/05 before:w-full before:h-full before:z-[-1] before:content-[""] before:absolute before:rounded-[inherit] before:left-0 before:top-0;
 }
+
 .tooltip.borderThick.top {
   @apply border-b-gray-800 border-b-4 border-solid after:bottom-[-7px];
 }
+
 .tooltip.borderThick.left {
   @apply border-r-gray-800 border-r-4 border-solid after:right-[-7px];
 }
+
 .tooltip.borderThick.right {
   @apply border-l-gray-800 border-l-4 border-solid after:left-[-7px];
 }
+
 .tooltip.borderThick.bottom {
   @apply border-t-gray-800 border-t-4 border-solid after:top-[-7px];
 }
+
 .tooltip.borderThick:before {
   background: inherit;
 }
+
 .tooltip.borderThick:after {
   @apply !bg-primary
 }
+
 .tooltip.border {
   @apply bg-white text-gray-800 border-gray-800 border-2 border-solid before:w-full before:h-full before:z-[-1] before:content-[""] before:absolute before:rounded-[inherit] before:left-0 before:top-0 after:border-gray-800 after:w-3 after:h-3 after:border-2 after:border-solid;
 }
+
 .tooltip.border:before {
   background: inherit;
 }
+
 .tooltip.circle {
   @apply rounded-[20px];
 }
+
 .tooltip.square {
   @apply rounded-none;
 }
+
 .tooltip.notArrow:after {
   @apply hidden;
 }
+
 .tooltip.shadow {
   @apply bg-white text-gray-800 shadow-[0_2px_10px_0] shadow-black/15 before:w-full before:h-full before:z-[-1] before:content-[""] before:absolute before:rounded-[inherit] before:left-0 before:top-0 after:shadow-[0_2px_10px_0] shadow-black/05;
 }
+
 .tooltip.shadow:before {
   @apply !bg-white
 }
+
 .tooltip.shadow:after {
   @apply !bg-white shadow-[0_2px_10px_0] shadow-black/15
 }
+
 .tooltip.isDark {
   @apply text-white bg-gray-200;
 }
+
 .tooltip.top:after {
   @apply bottom-[-3px] rounded-[0_0_2px_0];
 }
+
 .tooltip.bottom:after,
 .tooltip.top:after {
   @apply bg-gray-800 content-[""] absolute w-2.5 h-2.5 -translate-x-2/4 rotate-45 z-[-2] left-2/4;
 }
+
 .tooltip.bottom:after {
   @apply top-[-3px] rounded-[2px_0_0_0];
 }
+
 .tooltip.left:after {
   @apply right-[-3px] rounded-[0_2px_0_0];
 }
+
 .tooltip.left:after,
 .tooltip.right:after {
   @apply content-[""] absolute w-2.5 h-2.5 -translate-y-2/4 rotate-45 z-[-2] top-2/4;
   background: inherit;
 }
+
 .tooltip.right:after {
   @apply left-[-3px] rounded-[0_0_0_2px];
 }
 
 
 @keyframes loadingTooltip {
-    0% {
-        -webkit-transform: rotate(0);
-        transform: rotate(0)
-    }
+  0% {
+    -webkit-transform: rotate(0);
+    transform: rotate(0)
+  }
 
-    to {
-        -webkit-transform: rotate(1turn);
-        transform: rotate(1turn)
-    }
+  to {
+    -webkit-transform: rotate(1turn);
+    transform: rotate(1turn)
+  }
 }
-
 </style>
--- plugins/tooltip.ts
+++ plugins/tooltip.ts
@@ -1,45 +1,25 @@
 import Tooltip from '~/components/Tooltip.vue'
-import { h, render, createVNode } from 'vue'
+import { h, render } from 'vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('tip', {
-    mounted(el, binding, vnode) {
+    mounted(el, binding) {
       if (!binding.value) return; // Ne rien faire si aucune valeur n'est passée
-
-      // Cloner l'élément pour conserver ses styles et attributs
-      const parent = el.cloneNode(false); // Ne pas cloner les enfants
-      el.replaceWith(parent);
-
-      // Détecter les enfants dans vnode
-      let children;
-      if (Array.isArray(vnode.children)) {
-        // Cas où les enfants sont déjà un tableau
-        children = vnode.children;
-      } else if (typeof vnode.children === 'function') {
-        // Cas où les enfants sont une fonction (slot dynamique)
-        children = vnode.children();
-      } else if (typeof vnode.children === 'string' || typeof vnode.children === 'object') {
-        // Cas où les enfants sont une chaîne ou un objet unique
-        children = [vnode.children];
-      } else {
-        // Aucun enfant ou format inattendu
-        children = [];
-      }
 
       // Créer le composant Tooltip
       const tooltipElement = h(
         Tooltip,
-        {},
         {
-          default: () =>
-            createVNode(vnode.type, vnode.props || {}, children), // Utiliser les enfants traités
+          viaDirective: true,
+          focusedElement: el
+        },
+        {
           tooltip: () => binding.value, // Passer la valeur de la directive comme prop
         }
       );
-      el.remove()
 
       // Rendu du Tooltip dans le conteneur cloné
-      render(tooltipElement, parent);
+      render(tooltipElement, document.body);
     },
   });
 });
