57229c4025bb25d780a3734a98997cbc93507829
Vincent GUILLOIS <vincent@helloasso.org>
2025-01-26 13:39:19 +0100

    fix input label

--- components/Input.vue
+++ components/Input.vue
@@ -8,7 +8,7 @@
     <div
       :class="['input-content rounded-none flex items-center justify-start relative rounded-xl', { 'mt-5': label || labelPlaceholder }]">
 
-      <input v-if="!colorPicker" :disabled="loading || disabled" :class="['input  placeholder-transparent  transition-all duration-[0.25s] ease-[ease] w-50 pl-3 pr-[13px] py-[7px] rounded-[inherit] border-2 border-solid border-transparent focus:pl-[15px] min-h-10 outline-none box-border list-none',
+      <input v-if="!colorPicker" :disabled="loading || disabled" :class="['input text-gray-800 placeholder-transparent  transition-all duration-[0.25s] ease-[ease] w-50 pl-3 pr-[13px] py-[7px] rounded-[inherit] border-2 border-solid border-transparent focus:pl-[15px] min-h-10 outline-none box-border list-none',
     {
       'input--has-icon': !!icon || attrs.type === 'color',
       'input--has-icon--after': !!iconAfter,
@@ -34,7 +34,6 @@
       'text-red-500': state == 'danger',
       'text-yellow-500': state == 'warn',
       'text-primary': state == 'primary',
-      'input__label--hidden opacity-0 invisible': modelValue !== ''
     }
   ]">
         {{ label || attrs.placeholder || labelPlaceholder }}
